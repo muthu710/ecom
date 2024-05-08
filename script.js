e1e4cde2645252f789ebde2749802c7345f4e692
muthu710 <169181031+muthu710@users.noreply.github.com>
2024-05-08 07:32:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var sideNavbar = document.querySelector(".side-navbar");
+function showNavbar() {
+
+    sideNavbar.style.left = "0"
+}
+function closeNavbar(){
+    sideNavbar.style.left = "-60%"
+
+}
